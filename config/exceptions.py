842b1ea2570d5273f0bf82699b2b78c003e0d170
linda2927 <linda2927@naver.com>
2023-07-06 15:24:08 +0900

    feat: add custom exception classes

---
+++ config/exceptions.py
@@ -0,0 +1,23 @@
+from ninja import Schema
+from ninja_extra import status
+from ninja_extra.exceptions import APIException
+
+
+class ExceptionSchema(Schema):
+    message: str
+    status_code: int
+
+
+class BadRequestException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    message = "Bad request"
+
+
+class DuplicateInstanceException(APIException):
+    status_code = status.HTTP_409_CONFLICT
+    message = "Duplicate instance"
+
+
+class UnprocessableEntityException(APIException):
+    status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+    message = "Unprocessable entity"
