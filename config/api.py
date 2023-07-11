857eaa78cb20b4e0d0c4905225142aafc09c751a
linda2927 <linda2927@naver.com>
2023-07-11 14:32:08 +0900

    feat: add custom renderer for ninja app

--- config/api.py
+++ config/api.py
@@ -1,9 +1,11 @@
+from ninja.errors import ValidationError
 from ninja_extra import NinjaExtraAPI
 
 from apps.customers.api import CustomerController
 from apps.products.api import ProductController
 from apps.schedules.api import ScheduleController
 from apps.users.auth_api import AuthController
+from config.renderer import ResponseRenderer
 
 api = NinjaExtraAPI(
     openapi_extra={
@@ -17,9 +19,15 @@ api = NinjaExtraAPI(
     servers=[
         {"url": "http://localhost:8000", "description": "Development Local Server"}
     ],
+    renderer=ResponseRenderer(),
 )
 
 api.register_controllers(AuthController)
 api.register_controllers(CustomerController)
 api.register_controllers(ScheduleController)
 api.register_controllers(ProductController)
+
+
+@api.exception_handler(ValidationError)
+def custom_validation_errors(request, exc):
+    return api.create_response(request, {"detail": exc.errors}, status=400)
