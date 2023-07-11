b63966ce55240a7c97697ea17333a1ba07d2dfc5
linda2927 <linda2927@naver.com>
2023-07-11 11:34:57 +0900

    fix: add fc field for CustomerSchedule model

--- apps/schedules/models.py
+++ apps/schedules/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from apps.customers.models import Customer
+from apps.users.models import User
 from config.mixins import TimestampMixin
 
 
@@ -13,6 +14,7 @@ class CustomerSchedule(TimestampMixin):
     has_end_time = models.BooleanField(default=False)
     start_time = models.TimeField(null=True)
     end_time = models.TimeField(null=True)
+    fc = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
 
     class Meta:
         db_table = "customer_schedule"
